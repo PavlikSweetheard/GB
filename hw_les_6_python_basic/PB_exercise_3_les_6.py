9207e9d3f2d4c0e96b5804ba9fe06ee4d5729083
Pavlik <pavliksweetheard@mail.ru>
2021-07-18 15:16:26 +0500

    add hw_6

---
+++ hw_les_6_python_basic/PB_exercise_3_les_6.py
@@ -0,0 +1,37 @@
+"""
+
+3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+name, surname, position (должность), income (доход).
+Последний атрибут должен быть защищенным и ссылаться на словарь,
+содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+Создать класс Position (должность)
+на базе класса Worker.
+В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+и дохода с учетом премии (get_total_income).
+Проверить работу примера на реальных данных
+(создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+
+"""
+
+
+class Worker:
+    def __init__(self, worker_name, worker_surname, worker_position, worker_wage, worker_bonus):
+        self.name = worker_name
+        self.surname = worker_surname
+        self.position = worker_position
+        self._income = {"wage": worker_wage, "bonus": worker_bonus}
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        print(f"Полное имя сотрудника: {self.name} {self.surname} {self.position}")
+
+    def get_total_income(self):
+        print(f"Доход с учетом премии сотрудника: {sum(self._income.values())}")
+
+
+new = Position('Ivan', 'Ivanov', 'senior', 150000, 500000)
+new.get_total_income()
+new.get_full_name()
